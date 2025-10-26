13d21595797233a2707dc5b11282ca63718407f7
jonms90 <s231866@gmail.com>
2025-10-26 20:47:30 +0100

    Change from minimal API to Controllers

---
+++ src/AspNetDemos.API/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetDemos.API;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = 
+        ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public ActionResult<WeatherForecast[]> Get()
+    {
+        WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                Summaries[Random.Shared.Next(Summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    }
+
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
